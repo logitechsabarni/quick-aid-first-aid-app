1785dc05a55c5ba5b274ebe744b4aeb47f0967b1
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-21 00:43:03 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import { useEffect } from "react";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
-  );
-};
+function App() {
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/hello`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Data from backend:", data);
+      });
+  }, []);
+
+  return <h1>QuickAid</h1>;
+}
 
 export default App;
