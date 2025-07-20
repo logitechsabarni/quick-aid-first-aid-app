2b3f19122b15625eaf34f2aaf96c8b9e0a2b7504
Sabarni Guha <sabarni.guha2024@uem.edu.in>
2025-07-21 00:02:40 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+module.exports = {
+  content: [
+  "./client/index.html",
+  "./client/src/**/*.{js,ts,jsx,tsx}"
+]
+
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
